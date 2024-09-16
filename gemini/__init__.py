49ce2ba25e7ce716d1fb409f0314d185bb5709a1
Hunter Stone <hunter.m.stone@gmail.com>
2024-09-15 18:12:48 -0600

    Added config file for model parameters

--- gemini/__init__.py
+++ gemini/__init__.py
@@ -1,5 +1,11 @@
 import google.generativeai as genai
 import os
+import configparser
+
+# load prameters from config file
+config = configparser.ConfigParser()
+config.read('config.cfg')
+temp = float(config['gemini']['Temperature'])
 
 # initialize Gemini 1.5 Pro
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
@@ -7,5 +13,10 @@ model = genai.GenerativeModel("gemini-1.5-pro")
 
 # send a request to the model and return the text response
 def generate(input):
-    res = model.generate_content(input)
+    res = model.generate_content(
+        input,
+        generation_config = genai.GenerationConfig(
+            temperature=temp,
+        )
+    )
     return res.text
